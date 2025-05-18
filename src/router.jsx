bb31d36310f9a07c93430255b847346a1a459f97
vaishali03jan <kshitijsoni469@gmail.com>
2025-05-18 17:49:34 +0530

    changes in servers

--- src/router.jsx
+++ src/router.jsx
@@ -1,4 +1,3 @@
-import { phoneData } from "./data/phoneData";
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import Home from "./pages/Home";
@@ -9,37 +8,39 @@ import Checkout from "./pages/Checkout";
 import OrderConfirmation from "./pages/OrderConfirmation";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-import Search from "./pages/Search";
 import Category from "./pages/Category";
 import SmartphoneDeals from "./components/SmartphoneDeals";
 import PhoneDetail from "./pages/PhoneDetail";
 import ProductView from "./pages/ProductView";
 import CategoryList from "./components/CategoryList";
-import SearchPage from './pages/SearchPage';
+import SearchPage from "./pages/SearchPage";
 
-
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+      children: [
+        { index: true, element: <Home /> },
+        { path: "products", element: <Products /> },
+        { path: "product/:slug", element: <ProductDetail /> },
+        { path: "category/:name", element: <Category /> },
+        { path: "cart", element: <Cart /> },
+        { path: "checkout", element: <Checkout /> },
+        { path: "order-confirmation", element: <OrderConfirmation /> },
+        { path: "about", element: <About /> },
+        { path: "contact", element: <Contact /> },
+        { path: "", element: <SmartphoneDeals /> },
+        { path: "phones/:slug", element: <PhoneDetail /> },
+        { path: "product-view/:id", element: <ProductView /> },
+        { path: "categories", element: <CategoryList /> },
+        { path: "search", element: <SearchPage /> },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <App />,
-    children: [
-      { index: true, element: <Home /> },
-      { path: "products", element: <Products /> },
-      { path: "product/:slug", element: <ProductDetail /> },
-      { path: "category/:name", element: <Category /> },
-      { path: "cart", element: <Cart /> },
-      { path: "checkout", element: <Checkout /> },
-      { path: "order-confirmation", element: <OrderConfirmation /> },
-      { path: "about", element: <About /> },
-      { path: "contact", element: <Contact /> },
-      // { path: "search", element: <Search /> },
-      { path: "", element: <SmartphoneDeals /> },
-      { path: "phones/:slug", element: <PhoneDetail /> },
-      { path: "product-view/:id", element: <ProductView /> },
-      { path: "categories", element: <CategoryList /> },
-      {path: "/search", element: <SearchPage />}
-    ],
-  },
-]);
+    basename: "/ecommerce-app", // ✅ required for GitHub Pages routing
+  }
+);
 
 export default router;
